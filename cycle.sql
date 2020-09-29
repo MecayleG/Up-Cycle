055dce04c59779c2c3aebcb849c3e6daa6e94f28
Amirah <amirahdavids2503@gmail.com>
2020-09-29 15:42:14 +0200

    added test file

--- cycle.sql
+++ cycle.sql
@@ -3,6 +3,6 @@ drop table if exists recycle;
 create table recycle (
     id  serial not null primary key,
     material text not null,
-    price(per kg) int not null,
+    price decimal not null,
     quantity int not null
 );
